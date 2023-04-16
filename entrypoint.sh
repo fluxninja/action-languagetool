2cdf2530747ef66ede783b144c91f9d70d0dbe04
Harjot Gill <harjot.gill@fluxninja.com>
2023-04-15 23:41:47 -0700

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -121,5 +121,5 @@ markup_to_json() {
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-run_langtool |
+run_langtool | tee /dev/stderr |
 	reviewdog -efm="%A%f:%l:%c: %m" -efm="%C %m" -name="LanguageTool" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
